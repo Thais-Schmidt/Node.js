fa9093a852734720224c35b34ce31547e39261a8
Thais Schmidt <thaisachmidtdasilva@gmail.com>
2023-11-23 21:45:24 -0300

    atualizando aula de express-handlebars

--- 10express_handlebars/app.js
+++ 10express_handlebars/app.js
@@ -1,25 +1,31 @@
 // import express from 'express';
 const express = require('express');
 // import { engine } from 'express-handlebars';
-const {engine} = require('express-handlebars');
+const { engine } = require('express-handlebars');
 
 const app = express();
 const port = 8080;
 
-app.engine('handlebars', engine({defaultLayout:('main')}));
+app.engine('handlebars', engine({ defaultLayout: ('main') }));
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
 app.use(express.static(`${__dirname}/publico`));
 
 app.get('/', (req, res) => {
-    res.render('home');
+    let pessoas = [
+    { nome: `Thais`, idade: 22 },
+    { nome: `Gio`, idade: 22 },
+    { nome: `Valle`, idade: 20 },
+    { nome: `Tiago`, idade: 19 },
+];
+    res.render('home', { expressHndBrs: true, dados: pessoas });
 });
 
 app.get('/about', (req, res) => {
     res.render('about');
 });
 
-app.listen(port,() => {
+app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}.`);
 });
